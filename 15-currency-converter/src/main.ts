654cb019046ce60ceba9b2f32096cd8540a07444
Alim Nagoev <nagoev.personal@gmail.com>
2023-09-12 20:20:58 +0300

    Add 15-currency-converter

---
+++ 15-currency-converter/src/main.ts
@@ -0,0 +1,224 @@
+import './style.css'; // Импорт стилей
+import Toastify from 'toastify-js'; // Импорт библиотеки для уведомлений
+import 'toastify-js/src/toastify.css'; // Импорт стилей для уведомлений
+import countries from './mock'; // Импорт данных о странах
+import feather from 'feather-icons'; // Импорт иконок
+import axios from 'axios'; // Импорт библиотеки Axios для выполнения HTTP-запросов
+
+// Интерфейс, описывающий структуру ответа от сервера, содержащего информацию о курсе обмена
+interface ExchangeRateResponse {
+  result: number;
+  date: string;
+  success: boolean;
+  info: {
+    rate: number;
+  };
+}
+
+class CurrencyConverter {
+  // Приватные поля для хранения ссылок на элементы DOM
+  private form: HTMLFormElement | null = null;
+  private selects: NodeListOf<HTMLSelectElement> | null = null;
+  private exchange: HTMLDivElement | null = null;
+  private switch: HTMLDivElement | null = null;
+
+  // Конструктор класса
+  constructor() {
+    this.initialize();
+  }
+
+  // Метод инициализации
+  private initialize() {
+    this.createDOM(); // Создание структуры DOM
+    this.setupEventListeners(); // Настройка обработчиков событий
+  }
+
+  // Метод для создания структуры DOM
+  private createDOM() {
+    const root = document.querySelector<HTMLDivElement>('#app');
+    if (!root) return;
+
+    root.innerHTML = `
+      <div class='border shadow rounded max-w-md w-full p-3 grid gap-4'>
+        <h1 class='text-center font-bold text-2xl md:text-4xl'>Currency Converter</h1>
+        <form class='grid gap-3' data-form>
+          <label class='grid gap-1'>
+            <span class='text-sm font-medium'>Enter Amount</span>
+            <input class='border-2 rounded px-3 py-2.5 bg-gray-50 focus:outline-none focus:border-blue-400' type='number' value='1' step='1' min='1' name='amount'>
+          </label>
+          <div class='grid gap-3 sm:grid-cols-[auto_40px_auto] sm:items-end'>
+            <label>
+              <span class='text-sm font-medium'>From</span>
+              <div class='relative'>
+                <img class='absolute w-8 top-1/2 left-2 transform -translate-y-1/2' src='https://flagcdn.com/48x36/us.png' alt='flag'>
+                <select class='border-2 rounded px-3 py-2.5 pl-10 bg-gray-50 focus:outline-none focus:border-blue-400 w-full' data-select='from' name='from'>
+                  ${countries.map(({ name }) => `${name === 'USD' ? `<option value='${name}' selected>${name}</option>` : `<option value='${name}'>${name}</option>`}`).join('')}
+                </select>
+              </div>
+            </label>
+            <div class='border p-2 rounded flex justify-center cursor-pointer hover:bg-neutral-100 transition sm:border-none sm:mb-1.5' data-switch>${feather.icons.repeat.toSvg()}</div>
+            <label>
+              <span class='text-sm font-medium'>To</span>
+              <div class='relative'>
+                <img class='absolute w-8 top-1/2 left-2 transform -translate-y-1/2' src='https://flagcdn.com/48x36/ru.png' alt='flag'>
+                <select class='border-2 rounded px-3 py-2.5 pl-10 bg-gray-50 focus:outline-none focus:border-blue-400 w-full'  data-select='to' name='to'>
+                  ${countries.map(({ name }) => `${name === 'RUB' ? `<option value='${name}' selected>${name}</option>` : `<option value='${name}'>${name}</option>`}`).join('')}
+                </select>
+              </div>
+            </label>
+          </div>
+
+          <div class='hidden' data-exchange>Getting exchange rate...</div>
+          <button class='border px-3 py-2.5 hover:bg-neutral-100' type='submit'>Get Exchange Rate</button>
+        </form>
+      </div>
+    `;
+
+    this.form = document.querySelector('[data-form]') as HTMLFormElement;
+    this.selects = document.querySelectorAll('[data-select]') as NodeListOf<HTMLSelectElement>;
+    this.exchange = document.querySelector('[data-exchange]') as HTMLDivElement;
+    this.switch = document.querySelector('[data-switch]') as HTMLDivElement;
+  }
+
+  // Метод для настройки обработчиков событий
+  private setupEventListeners() {
+    if (!this.form || !this.selects || !this.switch) return;
+    this.getExchange();
+    this.form.addEventListener('submit', this.handleSubmit.bind(this));
+    this.selects.forEach(select => select.addEventListener('change', this.handleChange.bind(this)));
+    this.switch.addEventListener('click', this.handleSwitch.bind(this));
+  }
+
+  // Метод для получения курса обмена валюты.
+  private getExchange() {
+    if (!this.form || !this.selects || !this.exchange) return;
+    const amountInput = this.form.querySelector('[type="number"]') as HTMLInputElement | null;
+    if (!amountInput) return;
+    this.exchange.classList.remove('hidden');
+    this.fetchData(amountInput.value, this.selects[0].value, this.selects[1].value);
+  }
+
+  // Метод для обработки отправки формы.
+  private handleSubmit(event: Event) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const entries = formData.entries();
+    const data: Record<string, string> = {};
+    for (const [key, value] of entries) {
+      data[key] = value;
+    }
+    const { amount, to, from } = data;
+
+    if (amount.trim().length === 0 || !to || !from) {
+      Toastify({
+        text: '⛔️ Please fill the fields',
+        className: 'bg-none shadow-none bg-orange-100 text-black border border-orange-200',
+        duration: 3000,
+        gravity: 'bottom',
+        position: 'center',
+      }).showToast();
+      return;
+    }
+
+    this.fetchData(amount, from, to);
+  }
+
+  // Метод для обработки изменения выбора валюты.
+  private handleChange(event: Event) {
+    const target = event.target as HTMLSelectElement | null;
+    if (!target) return;
+    const flagImg = target.previousElementSibling as HTMLImageElement;
+    const selectedCountry = countries.find(({ name }) => name === target.value);
+    if (selectedCountry) {
+      const flagAbbr = selectedCountry.value.toLowerCase();
+      flagImg.src = `https://flagcdn.com/48x36/${flagAbbr}.png`;
+    }
+  }
+
+  //  Метод для обработки переключения валют.
+  private handleSwitch() {
+    if (!this.selects) return;
+    const amountInput = this.form?.querySelector('[type="number"]') as HTMLInputElement | null;
+    if (!amountInput) return;
+    const amount = amountInput.value;
+    const from = this.selects[0];
+    const to = this.selects[1];
+    const tmpCode = from.value;
+    if (from && from.previousElementSibling && to && to.previousElementSibling) {
+      const tmpSrc = (from.previousElementSibling as HTMLImageElement).src;
+      if (amount.trim().length === 0) {
+        Toastify({
+          text: '⛔️ Please fill the fields.',
+          className: 'bg-none shadow-none bg-orange-100 text-black border border-orange-200',
+          duration: 3000,
+          gravity: 'bottom',
+          position: 'center',
+        }).showToast();
+        return;
+      }
+      from.value = to.value;
+      to.value = tmpCode;
+      (from.previousElementSibling as HTMLImageElement).src = (to.previousElementSibling as HTMLImageElement).src;
+      (to.previousElementSibling as HTMLImageElement).src = tmpSrc;
+
+      this.fetchData(amount, from.value, to.value);
+    }
+
+  }
+
+  // Метод для выполнения HTTP-запроса и получения данных о курсе обмена.
+  private async fetchData(amount: string, from: string, to: string) {
+    if (!this.exchange) return;
+    try {
+      this.exchange.classList.remove('hidden');
+      this.exchange.innerHTML = 'Getting exchange rate...';
+      const response = await axios.get<ExchangeRateResponse>(`https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`);
+      const {
+        result,
+        date,
+        success,
+        info: { rate },
+      } = response.data;
+
+      if (!success) {
+        this.message(' Something went wrong, open dev console.');
+        return;
+      }
+
+      this.exchange.innerHTML = `
+      <table class='table-auto w-full'>
+        <tr>
+          <td class='border p-2 bg-neutral-50 font-medium'><span>Date</span></td>
+          <td class='border p-2'>${new Date(date).toLocaleString()}</td>
+        </tr>
+        <tr>
+          <td class='border p-2 bg-neutral-50 font-medium'><span>Rate</span></td>
+          <td class='border p-2'><span>1</span> ${from} = <span>${rate.toFixed(2)}</span> ${to}</td>
+        </tr>
+        <tr>
+          <td class='border p-2 bg-neutral-50 font-medium'><span>Exchange</span></td>
+          <td class='border p-2'><span>${amount}</span> ${from} = <span>${result.toFixed(2)}</span> ${to}</td>
+        </tr>
+      </table>
+      `;
+    } catch (e) {
+      console.log(e);
+      this.message(' Something went wrong, open dev console.');
+    }
+  }
+
+  // Метод для отображения уведомлений.
+  private message(text: string, classname: string = 'bg-none shadow-none bg-orange-100 text-black border border-orange-200') {
+    Toastify({
+      text: `⛔ ${text}`,
+      className: classname,
+      duration: 3000,
+      gravity: 'bottom',
+      position: 'center',
+    }).showToast();
+  }
+}
+
+// Создание экземпляра класса CurrencyConverter для запуска приложения
+new CurrencyConverter();
---
+++ 15-currency-converter/src/mock/index.ts
@@ -0,0 +1,320 @@
+export default [
+  { name: 'AED', value: 'AE' },
+  { name: 'AFN', value: 'AF' },
+  { name: 'XCD', value: 'AG' },
+  { name: 'ALL', value: 'AL' },
+  { name: 'AMD', value: 'AM' },
+  { name: 'ANG', value: 'AN' },
+  { name: 'AOA', value: 'AO' },
+  { name: 'AQD', value: 'AQ' },
+  { name: 'ARS', value: 'AR' },
+  { name: 'AUD', value: 'AU' },
+  { name: 'AZN', value: 'AZ' },
+  { name: 'BAM', value: 'BA' },
+  { name: 'BBD', value: 'BB' },
+  { name: 'BDT', value: 'BD' },
+  { name: 'XOF', value: 'BE' },
+  { name: 'BGN', value: 'BG' },
+  { name: 'BHD', value: 'BH' },
+  { name: 'BIF', value: 'BI' },
+  { name: 'BMD', value: 'BM' },
+  { name: 'BND', value: 'BN' },
+  { name: 'BOB', value: 'BO' },
+  { name: 'BRL', value: 'BR' },
+  { name: 'BSD', value: 'BS' },
+  { name: 'NOK', value: 'BV' },
+  { name: 'BWP', value: 'BW' },
+  { name: 'BYR', value: 'BY' },
+  { name: 'BZD', value: 'BZ' },
+  { name: 'CAD', value: 'CA' },
+  { name: 'CDF', value: 'CD' },
+  { name: 'XAF', value: 'CF' },
+  { name: 'CHF', value: 'CH' },
+  { name: 'CLP', value: 'CL' },
+  { name: 'CNY', value: 'CN' },
+  { name: 'COP', value: 'CO' },
+  { name: 'CRC', value: 'CR' },
+  { name: 'CUP', value: 'CU' },
+  { name: 'CVE', value: 'CV' },
+  { name: 'CYP', value: 'CY' },
+  { name: 'CZK', value: 'CZ' },
+  { name: 'DJF', value: 'DJ' },
+  { name: 'DKK', value: 'DK' },
+  { name: 'DOP', value: 'DO' },
+  { name: 'DZD', value: 'DZ' },
+  { name: 'ECS', value: 'EC' },
+  { name: 'EEK', value: 'EE' },
+  { name: 'EGP', value: 'EG' },
+  { name: 'ETB', value: 'ET' },
+  { name: 'EUR', value: 'FR' },
+  { name: 'FJD', value: 'FJ' },
+  { name: 'FKP', value: 'FK' },
+  { name: 'GBP', value: 'GB' },
+  { name: 'GEL', value: 'GE' },
+  { name: 'GGP', value: 'GG' },
+  { name: 'GHS', value: 'GH' },
+  { name: 'GIP', value: 'GI' },
+  { name: 'GMD', value: 'GM' },
+  { name: 'GNF', value: 'GN' },
+  { name: 'GTQ', value: 'GT' },
+  { name: 'GYD', value: 'GY' },
+  { name: 'HKD', value: 'HK' },
+  { name: 'HNL', value: 'HN' },
+  { name: 'HRK', value: 'HR' },
+  { name: 'HTG', value: 'HT' },
+  { name: 'HUF', value: 'HU' },
+  { name: 'IDR', value: 'ID' },
+  { name: 'ILS', value: 'IL' },
+  { name: 'INR', value: 'IN' },
+  { name: 'IQD', value: 'IQ' },
+  { name: 'IRR', value: 'IR' },
+  { name: 'ISK', value: 'IS' },
+  { name: 'JMD', value: 'JM' },
+  { name: 'JOD', value: 'JO' },
+  { name: 'JPY', value: 'JP' },
+  { name: 'KES', value: 'KE' },
+  { name: 'KGS', value: 'KG' },
+  { name: 'KHR', value: 'KH' },
+  { name: 'KMF', value: 'KM' },
+  { name: 'KPW', value: 'KP' },
+  { name: 'KRW', value: 'KR' },
+  { name: 'KWD', value: 'KW' },
+  { name: 'KYD', value: 'KY' },
+  { name: 'KZT', value: 'KZ' },
+  { name: 'LAK', value: 'LA' },
+  { name: 'LBP', value: 'LB' },
+  { name: 'LKR', value: 'LK' },
+  { name: 'LRD', value: 'LR' },
+  { name: 'LSL', value: 'LS' },
+  { name: 'LTL', value: 'LT' },
+  { name: 'LVL', value: 'LV' },
+  { name: 'LYD', value: 'LY' },
+  { name: 'MAD', value: 'MA' },
+  { name: 'MDL', value: 'MD' },
+  { name: 'MGA', value: 'MG' },
+  { name: 'MKD', value: 'MK' },
+  { name: 'MMK', value: 'MM' },
+  { name: 'MNT', value: 'MN' },
+  { name: 'MOP', value: 'MO' },
+  { name: 'MRO', value: 'MR' },
+  { name: 'MTL', value: 'MT' },
+  { name: 'MUR', value: 'MU' },
+  { name: 'MVR', value: 'MV' },
+  { name: 'MWK', value: 'MW' },
+  { name: 'MXN', value: 'MX' },
+  { name: 'MYR', value: 'MY' },
+  { name: 'MZN', value: 'MZ' },
+  { name: 'NAD', value: 'NA' },
+  { name: 'XPF', value: 'NC' },
+  { name: 'NGN', value: 'NG' },
+  { name: 'NIO', value: 'NI' },
+  { name: 'NPR', value: 'NP' },
+  { name: 'NZD', value: 'NZ' },
+  { name: 'OMR', value: 'OM' },
+  { name: 'PAB', value: 'PA' },
+  { name: 'PEN', value: 'PE' },
+  { name: 'PGK', value: 'PG' },
+  { name: 'PHP', value: 'PH' },
+  { name: 'PKR', value: 'PK' },
+  { name: 'PLN', value: 'PL' },
+  { name: 'PYG', value: 'PY' },
+  { name: 'QAR', value: 'QA' },
+  { name: 'RON', value: 'RO' },
+  { name: 'RSD', value: 'RS' },
+  { name: 'RUB', value: 'RU' },
+  { name: 'RWF', value: 'RW' },
+  { name: 'SAR', value: 'SA' },
+  { name: 'SBD', value: 'SB' },
+  { name: 'SCR', value: 'SC' },
+  { name: 'SDG', value: 'SD' },
+  { name: 'SEK', value: 'SE' },
+  { name: 'SGD', value: 'SG' },
+  { name: 'SKK', value: 'SK' },
+  { name: 'SLL', value: 'SL' },
+  { name: 'SOS', value: 'SO' },
+  { name: 'SRD', value: 'SR' },
+  { name: 'STD', value: 'ST' },
+  { name: 'SVC', value: 'SV' },
+  { name: 'SYP', value: 'SY' },
+  { name: 'SZL', value: 'SZ' },
+  { name: 'THB', value: 'TH' },
+  { name: 'TJS', value: 'TJ' },
+  { name: 'TMT', value: 'TM' },
+  { name: 'TND', value: 'TN' },
+  { name: 'TOP', value: 'TO' },
+  { name: 'TRY', value: 'TR' },
+  { name: 'TTD', value: 'TT' },
+  { name: 'TWD', value: 'TW' },
+  { name: 'TZS', value: 'TZ' },
+  { name: 'UAH', value: 'UA' },
+  { name: 'UGX', value: 'UG' },
+  { name: 'USD', value: 'US' },
+  { name: 'UYU', value: 'UY' },
+  { name: 'UZS', value: 'UZ' },
+  { name: 'VEF', value: 'VE' },
+  { name: 'VND', value: 'VN' },
+  { name: 'VUV', value: 'VU' },
+  { name: 'YER', value: 'YE' },
+  { name: 'ZAR', value: 'ZA' },
+  { name: 'ZMK', value: 'ZM' },
+  { name: 'ZWD', value: 'ZW' },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
